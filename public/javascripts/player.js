18a797483b264277cf91db7245f35a3e4ed70440
michael <behm.39@osu.edu>
2020-03-28 11:21:54 -0400

    Added simple keyboard and mouse control

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -10,6 +10,24 @@ class Player {
         Player.R = 40;
     }
 
+    update() {
+        if (keyIsDown('W'.charCodeAt(0))) {
+            this.pos.y += -2;
+        }
+        if (keyIsDown('A'.charCodeAt(0))) {
+            this.pos.x += -2;
+        }
+        if (keyIsDown('S'.charCodeAt(0))) {
+            this.pos.y += 2;
+        }
+        if (keyIsDown('D'.charCodeAt(0))) {
+            this.pos.x += 2;
+        }
+        let relativeMouse = createVector(mouseX, mouseY);
+        relativeMouse.sub(this.pos);
+        this.heading = atan2(relativeMouse.y, relativeMouse.x);
+    }
+
     draw() {
         stroke(255);
         strokeWeight(4);
--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -1,11 +1,20 @@
 let player;
 
-function setup () {
+function setup() {
     createCanvas(600, 600);
     player = new Player(width / 2, height / 2);
 }
 
 function draw() {
+    update();
+    render();
+}
+
+function update() {
+    player.update();
+}
+
+function render() {
     background(201);
 
     player.draw();
