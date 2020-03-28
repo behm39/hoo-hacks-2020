9f35f957f9620dc4062b5e1e6d3b72962eeed7b9
michael <behm.39@osu.edu>
2020-03-28 10:40:54 -0400

    Added gitignore

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hoo Hacks 2020 - Michael Behm' });
 });
 
 module.exports = router;
